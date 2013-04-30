6dc84e578983bb496269a514b5b6ba41af87ffe5
nyakuo <ynyakuo@gmail.com>
2013-05-01 00:04:52 +0900

    Anythingの追加
    Anything(候補選択型インタフェース)の追加

--- elisp/init.el
+++ elisp/init.el
@@ -164,6 +164,7 @@
   (auto-install-update-emacswiki-package-name t)
   (auto-install-compatibility-setup))
 
+
 ;; --------------------------------------------------
 ;; auto-complete-mode
 ;; 自動補完機能
@@ -180,6 +181,49 @@
 (when (require 'redo+ nil t)
   (global-set-key (kbd "C-.") 'redo)
 )
+
+;; --------------------------------------------------
+;; Anything (候補選択型インタフェース)
+
+(when (require 'anything nil t)
+  (setq
+   ;; 候補を表示するまでの時間
+   anything-idle-delay 0.3
+
+   ;; タイプして再描画するまでの時間
+   anything-input-idle-delay 0.2
+
+   ;; 候補の最大表示数
+   anything-candiate-number-limit 100
+
+   ;; 候補が多い時に体感速度を早くする
+   anything-quick-update t
+
+   ;; 候補選択ショートカットをアルファベットに
+   anything-enable-shortcuts 'alphabet)
+
+(when (require 'anything-config nil t)
+  (setq anything-su-or-sudo "sudo"))
+
+(require 'anything-match-plugin nil t)
+
+(when (and (executable-find "cmigemo")
+           (require 'migemo nil t))
+  (require 'anything-migemo nil t))
+
+(when (require 'anything-complete nil t)
+  ;; lispシンボルの補完候補の再検索時間
+  (anything-lisp-complete-symbol-set-timer 150))
+
+(require 'anything-show-completion nil t)
+
+(when (require 'auto-install nil t)
+  (require 'anything-auto-install nil t))
+
+(when (require 'descbinds-anything nil t)
+  ;; describe-bindingsをAnythingに置き換える
+  (descbinds-anything-install)))
+
 
 ;; --------------------------------------------------
 ;; verilog-mode 
