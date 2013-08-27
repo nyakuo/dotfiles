b9f6d40f7c7c56f591fd1e8c593753292b98dd09
nyakuo <ynyakuo@gmail.com>
2013-08-27 14:16:33 +0900

    フォント設定をもとに戻す
    変更が反映されていなかったのは，バイトコンパイルがされていないためだった

--- elisp/init.el
+++ elisp/init.el
@@ -111,6 +111,28 @@
   (color-theme-initialize)
   (color-theme-comidia))
 
+;; フォント設定
+;; Win: Meiryo
+;; Mac: Menlo
+;; X:   Ricty
+(let ((ws window-system))
+  (cond ((eq ws 'w64)
+         (set-face-attribute 'default nil
+                             :family "メイリオ"  ;; 英数
+                             :height 100)
+         (set-fontset-font nil 'japanese-jisx0208 (font-spec :family "メイリオ")))  ;; 日本語
+        ((eq ws 'ns)
+         (set-face-attribute 'default nil
+                             :family "Menlo"  ;; 英数
+                             :height 140)
+         (set-fontset-font nil 'japanese-jisx0208 (font-spec :family "Osaka")))    ;; 日本語
+        ((eq ws 'x)
+         (set-face-attribute 'default nil
+                             :family "Ricty" ;; 英数
+                             :height 130)
+         (set-fontset-font nil 'japanese-jisx0208 (font-spec :family "Ricty")))))   ;; 日本語
+
+
 ;; メニューバーの非表示
 (menu-bar-mode -1)
 
@@ -125,7 +147,7 @@
 (setq hl-line-face 'underline)
 
 ;; 背景の透過
-(set-frame-parameter nil 'alpha 90)
+;(set-frame-parameter nil 'alpha 90)
 
 ;; --------------------------------------------------
 ;; auto-install
