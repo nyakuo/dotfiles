6c7534ed3e81e0246a9cafbb1da50e76bcf68819
nyakuo <ynyakuo@gmail.com>
2019-09-15 22:15:30 +0900

    [Add] fish_prompt.fish を管理に追加
    プロンプトの表示を管理するファイル

--- init.sh
+++ init.sh
@@ -7,5 +7,6 @@ DIR=$(pwd)
 
 ln -s ${DIR}/vim/vimrc ~/.vimrc
 ln -s ${DIR}/fish/config.fish ~/.config/fish/config.fish
+ln -s ${DIR}/fish/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
 ln -s ${DIR}/tmux/tmux.conf ~/.tmux.conf
 ln -s ${DIR}/git/git_alias ~/.git_alias
