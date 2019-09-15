48cf0b77cb67fcc07f7f8c858a1263bb9820853c
nyakuo <ynyakuo@gmail.com>
2019-09-15 23:00:46 +0900

    [Fix] split-window の引数を修正

--- init.sh
+++ init.sh
@@ -10,3 +10,4 @@ ln -s ${DIR}/fish/config.fish ~/.config/fish/config.fish
 ln -s ${DIR}/fish/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
 ln -s ${DIR}/tmux/tmux.conf ~/.tmux.conf
 ln -s ${DIR}/git/git_alias ~/.git_alias
+ln -s ${DIR}/vifm/vifmrc ~/.config/vifm/vifmrc
