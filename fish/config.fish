e696755154edfc3e39ea1c4006c3d97e2ebaadbd
nyakuo <ynyakuo@gmail.com>
2019-10-09 13:03:11 +0900

    git branch の表示の定義を移動

--- fish/config.fish
+++ fish/config.fish
@@ -17,24 +17,6 @@ if [ -f '/usr/local/bin/google-cloud-sdk/path.fish.inc' ]; . '/usr/local/bin/goo
 # Go のバイナリの PATH を追加
 set PATH ~/go/bin $PATH
 
-# --- git のブランチ名を表示
-# Fish git prompt
-set __fish_git_prompt_showdirtystate 'yes'
-set __fish_git_prompt_showstashstate 'yes'
-set __fish_git_prompt_showuntrackedfiles 'yes'
-set __fish_git_prompt_showupstream 'yes'
-set __fish_git_prompt_color_branch yellow
-set __fish_git_prompt_color_upstream_ahead green
-set __fish_git_prompt_color_upstream_behind red
-
-# Status Chars
-set __fish_git_prompt_char_dirtystate '⚡'
-set __fish_git_prompt_char_stagedstate '→'
-set __fish_git_prompt_char_untrackedfiles '☡'
-set __fish_git_prompt_char_stashstate '↩'
-set __fish_git_prompt_char_upstream_ahead '+'
-set __fish_git_prompt_char_upstream_behind '-'
-
 # --- Mac 固有の設定
 if test (uname) = Darwin
   # MacVim のための設定
--- fish/fish_prompt.fish
+++ fish/fish_prompt.fish
@@ -1,5 +1,5 @@
 function fish_prompt --description 'Write out the prompt'
-	#Save the return status of the previous command
+  #Save the return status of the previous command
     set stat $status
 
     if not set -q __fish_prompt_normal
@@ -15,6 +15,24 @@ function fish_prompt --description 'Write out the prompt'
     if test $stat -gt 0
         set __fish_color_status (set_color -o red)
     end
+
+# --- git のブランチ名を表示
+# Fish git prompt
+    set __fish_git_prompt_showdirtystate 'yes'
+    set __fish_git_prompt_showstashstate 'yes'
+    set __fish_git_prompt_showuntrackedfiles 'yes'
+    set __fish_git_prompt_showupstream 'yes'
+    set __fish_git_prompt_color_branch yellow
+    set __fish_git_prompt_color_upstream_ahead green
+    set __fish_git_prompt_color_upstream_behind red
+
+# Status Chars
+    set __fish_git_prompt_char_dirtystate '⚡'
+    set __fish_git_prompt_char_stagedstate '→'
+    set __fish_git_prompt_char_untrackedfiles '☡'
+    set __fish_git_prompt_char_stashstate '↩'
+    set __fish_git_prompt_char_upstream_ahead '+'
+    set __fish_git_prompt_char_upstream_behind '-'
 
     switch "$USER"
 
