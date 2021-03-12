ce0ddea66bf3dc624775e215b0a37e9382b8dfe1
nyakuo <ynyakuo@gmail.com>
2021-03-13 00:11:18 +0900

    設定ファイルを vim から neovim 前提に変更
    * シンボリックリンクを ~/.vim から ~/.config/nvim/init.vim に変更
    * ディレクトリ名を vim から neovim に変更

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@
 
 DIR=$(pwd)
 
-ln -s ${DIR}/vim/vimrc ~/.vimrc
+ln -s ${DIR}/neovim/vimrc ~/.config/nvim/init.vim
 ln -s ${DIR}/fish/config.fish ~/.config/fish/config.fish
 ln -s ${DIR}/fish/extra_settings ~/.config/fish/extra_settings
 ln -s ${DIR}/fish/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
