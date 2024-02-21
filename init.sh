e7e15333f9438273150c04ce7d38ab08f2e444c1
nyakuo <ynyakuo@gmail.com>
2024-02-21 16:34:40 +0900

    del: fish を使わなくなったので関連ファイルを削除

--- init.sh
+++ init.sh
@@ -6,9 +6,6 @@
 DIR=$(pwd)
 
 ln -s ${DIR}/neovim/vimrc ~/.config/nvim/init.vim
-ln -s ${DIR}/fish/config.fish ~/.config/fish/config.fish
-ln -s ${DIR}/fish/extra_settings ~/.config/fish/extra_settings
-ln -s ${DIR}/fish/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
 ln -s ${DIR}/tmux/tmux.conf ~/.tmux.conf
 ln -s ${DIR}/git/git_alias ~/.git_alias
 ln -s ${DIR}/vifm/vifmrc ~/.config/vifm/vifmrc
