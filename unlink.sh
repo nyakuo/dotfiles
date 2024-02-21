29411f830d859cee36a821b82b3e2bf3228ed1ed
nyakuo <ynyakuo@gmail.com>
2024-02-21 16:32:54 +0900

    update: unlink.sh で一部のシンボリックリングが対象から漏れていたので追加

--- unlink.sh
+++ unlink.sh
@@ -3,12 +3,15 @@
 # 設定ファイルのシンボリックリンクを削除
 ########################################
 
+unlink ~/.config/nvim/init.vim
 unlink ~/.vimrc
-unlink ~/.config/fish/config.fish
-unlink ~/.config/fish/functions/fish_prompt.fish
 unlink ~/.tmux.conf
 unlink ~/.git_alias
 unlink ~/.config/vifm/vifmrc
 unlink ~/.xinitrc
 unlink ~/.config/fontconfig/fonts.conf
+unlink ~/.config/alacritty/alacritty.yml
+unlink ~/.config/xremap/config
+unlink ~/.emacs.d/init.el
+unlink ~/.zshrc
 
